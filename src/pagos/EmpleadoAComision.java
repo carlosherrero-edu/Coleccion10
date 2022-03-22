faba64b25e1d763c0fc5d0e06e70cde1cbd026b7
cherre1 <carloscherrero@yahoo.es>
2022-03-22 19:22:25 +0100

    CAmbios en clases del paquete pagos

--- src/pagos/EmpleadoAComision.java
+++ src/pagos/EmpleadoAComision.java
@@ -1,16 +1,59 @@
 package pagos;
 
 public class EmpleadoAComision extends Empleado {
+	
+	//propiedades
+	private double ventasSemanales;
+	private double comision;
 
-	public EmpleadoAComision(String nombre, String apellidos, String numSegSocial) {
+	public EmpleadoAComision(String nombre, String apellidos, String numSegSocial, double ventasSem, double comision) {
 		super(nombre, apellidos, numSegSocial);
-		// TODO Auto-generated constructor stub
+		try {
+			setVentasSemanales(ventasSem);
+			setComision (comision);
+			
+		} catch (IllegalArgumentException e) {
+			throw e;
+		}
 	}
+
+	public double getVentasSemanales() {
+		return ventasSemanales;
+	}
+
+	public void setVentasSemanales(double ventasSemanales) {
+		if ( ! (ventasSemanales>0)) {
+			throw new IllegalArgumentException("Las ventas deben ser mayores de 0");
+		}
+		this.ventasSemanales = ventasSemanales;
+	}
+
+	public double getComision() {
+		
+		return comision;
+	}
+
+	public void setComision(double comision) {
+		if (comision<0 || comision>100) {
+			throw new IllegalArgumentException("El porcentaje de comisión debe estar entre 0 y 100");
+		}
+		this.comision = comision;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return String.format("%s --- Ventas Semanales:%.2f -- Comisión: %.2f%% ", super.toString(),
+				getVentasSemanales(), getComision());
+	}
+
 
 	@Override
 	public double calculaImporteAPagar() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+			double importeAPagar;
+			importeAPagar= (getVentasSemanales()*getComision())/100;
+			return importeAPagar;
 	}
 
 }
--- src/pagos/EmpleadoPorHoras.java
+++ src/pagos/EmpleadoPorHoras.java
@@ -2,15 +2,62 @@ package pagos;
 
 public class EmpleadoPorHoras extends Empleado {
 
-	public EmpleadoPorHoras(String nombre, String apellidos, String numSegSocial) {
+	static final double FACTOR_HORA_EXTRA = 1.5;
+	static final double JORNADA_COMPLETA_SEMANAL = 40.0;
+	
+	// propiedades
+	private double horasSemanales;
+	private double tarifaHoraria;
+
+	public EmpleadoPorHoras(String nombre, String apellidos, String numSegSocial, double horasSemanales,
+			double tarifa) {
 		super(nombre, apellidos, numSegSocial);
-		// TODO Auto-generated constructor stub
+		try {
+			setHorasSemanales(horasSemanales);
+			setTarifaHoraria(tarifa);
+
+		} catch (IllegalArgumentException e) {
+			throw e;
+		}
+	}
+
+	public double getHorasSemanales() {
+		return horasSemanales;
+	}
+
+	public void setHorasSemanales(double horasSemanales) {
+		if (horasSemanales <= 0) {
+			throw new IllegalArgumentException("Las horas semanales tienen que ser más de 0");
+		}
+
+		this.horasSemanales = horasSemanales;
+	}
+
+	public double getTarifaHoraria() {
+		return tarifaHoraria;
+	}
+
+	public void setTarifaHoraria(double tarifaHoraria) {
+		if (tarifaHoraria <= 0) {
+			throw new IllegalArgumentException("La tarifa horaria debe ser mayor de 0");
+		}
+		this.tarifaHoraria = tarifaHoraria;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s --- Horas Semanales:%.2f -- Tarifa por horas:%.2f ", super.toString(),
+				getHorasSemanales(), getTarifaHoraria());
 	}
 
 	@Override
 	public double calculaImporteAPagar() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (getHorasSemanales() <= JORNADA_COMPLETA_SEMANAL) {
+			return getHorasSemanales() * getTarifaHoraria();
+		} else {
+			return JORNADA_COMPLETA_SEMANAL * getTarifaHoraria() + (JORNADA_COMPLETA_SEMANAL) * getTarifaHoraria() * FACTOR_HORA_EXTRA;
+		}
+
 	}
 
 }
